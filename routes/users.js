66aa46aa1048a2d48607e927990df6bf28c8aaf2
zantex94 <reneseer@gmail.com>
2022-02-22 12:57:48 +0100

    bugs

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require("bcryptjs");
 const TITLE = 'Todo';
-const con = require('../controllers/userController');
-const dep = require('../controllers/controller');
+const usercontroller = require('../controllers/userController');
+var cookieParser = require('cookie-parser')
+
+// app.use(cookieParser())
 
 /* GET users listing.  This is a DUMMY this route is caught in index.js */
 router.get('/', function(req, res, next) {
@@ -17,11 +20,6 @@ router.get('/register', async function(req, res, next) {
       departments
     });
 });
-/* POST handle form data for users */
-router.post('/register', function(req, res, next) {
-  con.postUser(req, res, next);                         // write user into db
-  res.redirect('/');                                    // go to front page
-});
 
 /* GET signup page */
 router.get('/signup', function(req, res, next) {
@@ -29,6 +27,11 @@ router.get('/signup', function(req, res, next) {
         title: TITLE,
         subtitle: 'Signup'
     });
+});
+/* Post signup page */
+router.post('/signup', function(req, res, next) {
+  usercontroller.postNewUser(req, res, next);                         // write user into db
+  res.redirect('/users/login');                                    // go to login page
 });
 /* GET signup page */
 router.get('/login', function(req, res, next) {
@@ -38,11 +41,36 @@ router.get('/login', function(req, res, next) {
     });
 });
 /* GET signup page */
-router.get('/admin', function(req, res, next) {
+router.post('/login', function(req, res, next) {
+
+});
+/* GET signup page */
+router.get('/admin', async function(req, res, next) {
+  let users = await usercontroller.getUser({});             // read users.
   res.render('admin', {
         title: TITLE,
-        subtitle: 'Activate/Deactivate users'
+        subtitle: 'Activate/Deactivate users',
+        users
     });
 });
+
+/* post admin page */
+router.get('admin/:username', async function(req, res, next) {
+  console.log(req.params.username)
+  // usercontroller.updateUser(req, res, next, 'active', req.params.username);
+  res.redirect('/users/admin'); 
+});
+/* post admin page */
+router.post('/admin1/:username', async function(req, res, next) {
+  console.log(req.params.username)
+  // usercontroller.updateUser(req, res, next, 'inactive', req.params.username);
+  res.redirect('/users/admin'); 
+});
+
+// // /* post admin page */
+// router.post("/:id/admin", function (req, res, next) {
+//   usercontroller.updateUser(req, res, next);
+//   res.redirect('/users/admin'); 
+// })
 
 module.exports = router;
